00132a7d6b049a1084cffaa5c12c331ea4ac6b6d
wz-Tian <twz974717170@gmail.com>
2025-11-16 16:40:36 +0200

    part1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Total = ({ course }) => {
+  return (
+    <p>
+      Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default App
